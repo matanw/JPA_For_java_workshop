b74c5ef1b88666891169cad1f28ebf65d1a37b87
Matan Wiesner <matanwiesner@google.com>
2022-10-27 16:31:03 +0300

    * add print all * add 3 entities

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
 import entity.Employee;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 public class Main {
   public static void main(String[] args) {
+
+    printAllEmployees();
+  }
+  private static void addOneEmployee(){
     Employee employee = new Employee();
     employee.setfName("Dalia");
     employee.setlName("Abo Sheasha");
@@ -14,6 +20,20 @@ public class Main {
     entityManager.getTransaction().commit();
     entityManager.close();
     entityManagerFactory.close();
+    System.out.println("ok!");
+  }
+  private static void printAllEmployees(){
+    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    TypedQuery<Employee> q = entityManager.createQuery("select e from Employee e", Employee.class);
+    List<Employee> employees = q.getResultList();
+    entityManager.close();
+    entityManagerFactory.close();
+
+    for (Employee e: employees){
+      System.out.println("e -"+e.getfName());
+    }
     System.out.println("ok!");
   }
 }
