1d662097d6b2aaaa42a2c9c552f617d6032305d8
Matan Wiesner <matanwiesner@google.com>
2022-10-30 08:41:46 +0200

    crud one entity

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,26 +4,56 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 public class Main {
   public static void main(String[] args) {
-    addOneEmployee();
+    printAllEmployees();
+    System.out.println("---");
+    updateById(2, "tamar");
+    updateById(6, "tamahr");
     printAllEmployees();
   }
   private static void addOneEmployee(){
     Employee employee = new Employee();
     employee.setName("Matan");
-    EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+    EntityManagerFactory entityManagerFactory = Persistence.
+        createEntityManagerFactory("default");
     EntityManager entityManager = entityManagerFactory.createEntityManager();
     entityManager.getTransaction().begin();
     entityManager.persist(employee);
     entityManager.getTransaction().commit();
     entityManager.close();
     entityManagerFactory.close();
-    System.out.println("ok!");
+  }
+  private static void deleteById(long id){
+    EntityManagerFactory entityManagerFactory = Persistence.
+        createEntityManagerFactory("default");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    Employee employee=entityManager.find(Employee.class, id);
+    entityManager.getTransaction().begin();
+    entityManager.remove(employee);
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    entityManagerFactory.close();
+  }
+  private static void updateById(long id,String newName){
+    EntityManagerFactory entityManagerFactory = Persistence.
+        createEntityManagerFactory("default");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+    Employee employee=entityManager.find(Employee.class, id);
+    employee.setName(newName);
+    entityManager.getTransaction().begin();
+    entityManager.persist(employee);
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    entityManagerFactory.close();
   }
   private static void printAllEmployees(){
-    for (Employee e: getAllEmployees()){
-      System.out.println("emp -"+e.getName());
+    for (Employee e: getAllEmployees2()){
+      System.out.println("emp -"+e.getId()+"-"+e.getName());
     }
   }
 
@@ -41,7 +71,14 @@ public class Main {
     EntityManagerFactory entityManagerFactory = Persistence.
         createEntityManagerFactory("default");
     EntityManager entityManager = entityManagerFactory.createEntityManager();
-    TypedQuery<Employee> q = entityManager.createQuery("select e from Employee e", Employee.class);
+
+    CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Employee> cq = cb.createQuery(Employee.class);
+    Root<Employee> from = cq.from(Employee.class);
+    cq.select(from);
+    TypedQuery<Employee> q = entityManager.createQuery(cq);
+
+
     List<Employee> result = q.getResultList();
     entityManager.close();
     entityManagerFactory.close();
